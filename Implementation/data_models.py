b413de1b2ba37c72b9b7b9ab13edbfb7b87fcd32
IoanaParfene <57050677+IoanaParfene@users.noreply.github.com>
2021-03-24 09:56:07 +0200

    Implemented different ways to reduce data

--- Implementation/data_models.py
+++ Implementation/data_models.py
@@ -1,8 +1,36 @@
 from sklearn.model_selection import train_test_split
 
 
-def split_data_by_articles():
-    pass
+def split_data_by_articles(vocabulary, word_frequency, merged_labeled_data, articles):
+    """ Reduce data to specified number of articles"""
+    counter = 0
+    new_merged_labeled_data, new_vocabulary, new_word_frequency = [], dict(), dict()
+    for key in vocabulary.keys():
+        if counter < articles:
+            new_merged_labeled_data.append(merged_labeled_data[counter])
+            new_vocabulary[key] = vocabulary[key]
+            new_word_frequency[key] = word_frequency[key]
+            counter += 1
+        else:
+            break
+    return new_merged_labeled_data, new_vocabulary, new_word_frequency
+
+
+def split_data_by_percentage(vocabulary, word_frequency, merged_labeled_data, percentage):
+    """ Reduce data to specified number of articles"""
+    counter = 0
+    max_counter = int(len(merged_labeled_data)/100*percentage)
+    new_merged_labeled_data, new_vocabulary, new_word_frequency = [], dict(), dict()
+    for key in vocabulary.keys():
+        if counter < max_counter:
+            new_merged_labeled_data.append(merged_labeled_data[counter])
+            new_vocabulary[key] = vocabulary[key]
+            new_word_frequency[key] = word_frequency[key]
+            counter += 1
+        else:
+            break
+    return new_merged_labeled_data, new_vocabulary, new_word_frequency
+
 
 
 def split_test_train_data(data, percentage):
